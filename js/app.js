3a7671d807fc6697f690fc7837846e49dfefbb2d
Maynard Williams <maymay.williams@gmail.com>
2016-04-18 11:09:41 -0700

    4/18/16

--- js/app.js
+++ js/app.js
@@ -23,16 +23,22 @@ $(document).ready(function() {
 	$('.ryu-throwing').hide();
 	$('.ryu-ready').show();
   });
+
+$(document).keydown(function (e) {
+    if (e.which == 88) {
+      $('.ryu-ready').hide();
+      $('.ryu-cool').show();
+    }
+  }).keyup(function () {
+    $('.ryu-cool').hide();
+    $('.ryu-ready').show();
+  });
+
 });
+
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
 }
-$('.ryu').keydown(function() {
-       $('.ryu-ready').hide();
-       $('.ryu-cool').show();     
-}).keyup(function() {
-       $('.ryu-cool').hide();
-       $('.ryu-still').show();
-});
+
